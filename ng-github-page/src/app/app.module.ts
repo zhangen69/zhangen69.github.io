192d5e7813c841fb820dc3761f4e203293e3c112
Jacob Tan <zhangen69@live.co.uk>
2019-09-23 16:36:06 +0800

    Update app.module.ts

--- ng-github-page/src/app/app.module.ts
+++ ng-github-page/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { RepoListComponent } from './repo/repo-list/repo-list.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -18,6 +20,9 @@ import { MaterialModule } from './material.module';
     HttpClientModule,
     MaterialModule,
     FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
