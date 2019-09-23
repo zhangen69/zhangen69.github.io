54182d8d91e80f0c9cc7c150fad2ded05cfc3d52
Jacob Tan <zhangen69@live.co.uk>
2019-09-23 16:53:02 +0800

    Update environment.prod.ts

--- ng-github-page/src/environments/environment.prod.ts
+++ ng-github-page/src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
+interface IApi {
+  baseUrl: string;
+  user: string;
+}
+
 export const environment = {
-  production: true
+  production: true,
+  api: {
+    baseUrl: 'https://api.github.com',
+    user: 'users/zhangen69',
+  },
+  getReposUrl: () => {
+    return `${environment.api.baseUrl}/${environment.api.user}/repos`;
+  }
 };
