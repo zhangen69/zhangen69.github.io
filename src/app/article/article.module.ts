e5bac01c32b3fc8f5c93f664df1a5938ac16a7d4
Jacob <zhangen69@gmail.com>
2019-12-25 14:46:33 +0800

    article

--- src/app/article/article.module.ts
+++ src/app/article/article.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ArticleListComponent } from './article-list/article-list.component';
 import { ArticleViewComponent } from './article-view/article-view.component';
-
+import { NzCardModule } from 'ng-zorro-antd/card';
 
 
 @NgModule({
   declarations: [ArticleListComponent, ArticleViewComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    NzCardModule,
   ]
 })
 export class ArticleModule { }
